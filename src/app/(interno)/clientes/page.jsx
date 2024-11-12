17471befcf7594c6191811927d87384b07b0e6c8
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2024-11-12 00:20:38 -0300

    ref: Alterando para MUIDataTable

--- src/app/(interno)/clientes/page.jsx
+++ src/app/(interno)/clientes/page.jsx
@@ -1,14 +1,11 @@
 "use client"
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Box } from '@mui/material';
+import dynamic from 'next/dynamic';
+import { Box, Tooltip, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
+const MUIDataTable = dynamic(() => import("mui-datatables"), { ssr: false });
 
 export default function Clientes() {
 
@@ -16,96 +13,96 @@ export default function Clientes() {
         return { name, celular, nascimento, status };
     }
 
-    const headersRanking = [
-        {
-            value: 'Nome',
-            objectValue: 'Nome',
-            align: 'left'
-        },
-        {
-            value: 'Celular',
-            objectValue: 'Telefone',
-            align: 'left'
-        },
-        {
-            value: 'Data Nascimento',
-            objectValue: 'DataNascimento',
-            align: 'left'
-        },
-        {
-            value: 'Status',
-            objectValue: 'Status',
-            align: 'left'
-        }
-    ]
-
     const rows = [
-        createData('Frozen yoghurt', '(11)98111-1111', '11/22/2323', 'INATIVO'),
-        createData('Ice cream sandwich', '(11)98111-1111', '11/22/2323', 'ATIVO'),
-        createData('Eclair', '(11)98111-1111', '11/22/2323', 'ATIVO'),
-        createData('Cupcake', '(11)98111-1111', '11/22/2323', 'ATIVO'),
-        createData('Gingerbread', '(11)98111-1111', '11/22/2323', 'INATIVO'),
+        createData('Valentino Hidromel', '(11)98111-1111', '11/22/2323', 'INATIVO'),
+        createData('Abner Coitadinho', '(11)98111-1111', '11/22/2323', 'ATIVO'),
+        createData('Vinicius porta choque', '(11)98111-1111', '11/22/2323', 'ATIVO'),
+        createData('Vlad dragão guerreiro x9', '(11)98111-1111', '11/22/2323', 'ATIVO'),
+        createData('Pebinha Lopes', '(11)98111-1111', '11/22/2323', 'INATIVO'),
+        createData('Pebinha Lopes2', '(11)98111-1111', '11/22/2323', 'INATIVO'),
     ];
 
-    const StatusBadge = ({ status }) => {
-        const isActive = status  === 'ATIVO';
-
-        return (
-            <Box
-                sx={{
-                    display: 'inline-block',
-                    padding: '4px 12px',
-                    borderRadius: '20px',
-                    fontSize: '0.875rem',
-                    fontWeight: 'bold',
-                    color: isActive ? '#FFFFFF' : '#A0A0A0', // Texto branco para ATIVO, cinza para INATIVO
-                    backgroundColor: isActive ? '#219653' : '#E0E0E0', // Verde para ATIVO, cinza claro para INATIVO
-                    textTransform: 'uppercase',
-                }}
-            >
-                {status}
-            </Box>
-        );
-    };
+    const columns = [
+        {
+            name: 'name',
+            label: 'Nome',
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: 'celular',
+            label: 'Celular',
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: 'nascimento',
+            label: 'Data Nascimento',
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: 'status',
+            label: 'Status',
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: 'actions',
+            label: 'Ações',
+            options: {
+                filter: false,
+                sort: false,
+                download: false,
+                customBodyRender: () => {
+                    return (
+                        <Box>
+                            <Tooltip title="Editar">
+                                <IconButton aria-label="edit" color="primary">
+                                    <EditIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Deletar">
+                                <IconButton aria-label="delete" color="error">
+                                    <DeleteIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                    );
+                }
+            }
+        }
+    ];
 
     return (
         <Box
-            bgcolor ="cdd0dc"
-            p = {8}
-            display = "flex"
-            justifyContent="center"
-            >
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow> 
-                            {
-                                headersRanking.map( row => (
-                                    <TableCell align="center">{row.value}</TableCell>
-                                ))
-                            }
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                hover
-                                sx={{ '&:hover': { border: 0, cursor: 'pointer' } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="center">{row.celular}</TableCell>
-                                <TableCell align="center">{row.nascimento}</TableCell>
-                                <TableCell align="center">
-                                    <StatusBadge status={row.status} />
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            p={15}
+            pt={8}
+            display="flex"
+            flexDirection="column"
+            alignItems="right"
+        >
+            <MUIDataTable
+                title={"Clientes"}
+                data={rows}
+                columns={columns}
+                options={{
+                    print: false,
+                    selectableRows: 'none',
+                    downloadOptions: {
+                        filename: 'clientes.csv',
+                        separator: ';',
+                    }
+                }}
+            />
         </Box>
 
     )
